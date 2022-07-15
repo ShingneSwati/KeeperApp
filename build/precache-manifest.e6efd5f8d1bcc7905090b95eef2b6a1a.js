fdd39ca9d87dc2c50e2995945a8332417ec0d499
Swati <swatishingne2@gmail.com>
2022-07-15 13:18:01 +0530

    First Commit

---
+++ build/precache-manifest.e6efd5f8d1bcc7905090b95eef2b6a1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b12a1e41fdaa59a4cd924b353b631da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0455d27905e66fa650d9",
+    "url": "/static/js/2.c61f6bc0.chunk.js"
+  },
+  {
+    "revision": "99a23c69f02ccfeca01b",
+    "url": "/static/js/main.0addeb80.chunk.js"
+  },
+  {
+    "revision": "05c790a9a60e4d4b4be2",
+    "url": "/static/js/runtime-main.a8757f30.js"
+  }
+]);
